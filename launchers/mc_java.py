e5261a6f2bb9c1dc04656d80e4dae0522f99d58b
Allen Sun <allen.haha@hotmail.com>
2024-07-10 15:20:19 +0800

    fix classmethod in mc_java

--- launchers/mc_java.py
+++ launchers/mc_java.py
@@ -7,19 +7,23 @@ from internetDriver import *
 CMCL_DOWNLOAD_URL = "https://github.com/MrShieh-X/console-minecraft-launcher/releases/download/latest/cmcl.jar"
 
 class MCLauncher(Launcher):
-    def install_cmcl(self):
+    @classmethod
+    def install_cmcl(cls):
         CreateDirIfInvalid(os.path.join(os.getcwd(), "downloads", "Minecraft_Java"))
         DownloadFile(CMCL_DOWNLOAD_URL, os.path.join(os.getcwd(), "downloads", "Minecraft_Java"))
-    def launch(self):
+    @classmethod
+    def launch(cls):
         pass
 
 class JavaManager():
-    def install_java_windows(version):
+    @classmethod
+    def install_java_windows(cls, version):
         CreateDirIfInvalid(os.path.join(os.getcwd(), "downloads", "Minecraft_Java"))
         DownloadFile(f"https://download.oracle.com/java/{version}/latest/jdk-{version}_windows-x64_bin.exe", os.path.join(os.getcwd(), "downloads", "Minecraft_Java"))
         os.start(os.path.join(os.getcwd(), "downloads", "Minecraft_Java", f"jdk-{version}_windows-x64_bin.exe"))
 
-    def install_java_macos(version):
+    @classmethod
+    def install_java_macos(cls, version):
         if platform.machine == "arm64":
             # runnning on arm64
             url = f"https://download.oracle.com/java/{version}/latest/jdk-{version}_macos-aarch64_bin.dmg"
