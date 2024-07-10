c6cd25745772a118b6fef4d8fb7c88f49307da44
Allen Sun <allen.haha@hotmail.com>
2024-07-10 15:09:52 +0800

    add java installer on windows

--- launchers/mc_java.py
+++ launchers/mc_java.py
@@ -4,6 +4,7 @@ import os.path
 from internetDriver import *
 
 CMCL_DOWNLOAD_URL = "https://github.com/MrShieh-X/console-minecraft-launcher/releases/download/latest/cmcl.jar"
+JAVA_DOWNLOAD_URL = "https://download.oracle.com/java/{version}/latest/jdk-{version}_windows-x64_bin.exe"
 
 class MCLauncher(Launcher):
     def install_cmcl(self):
@@ -11,3 +12,9 @@ class MCLauncher(Launcher):
         DownloadFile(CMCL_DOWNLOAD_URL, os.path.join(os.getcwd(), "downloads", "Minecraft_Java"))
     def launch(self):
         pass
+
+class JavaManager():
+    def install_java_windows(version):
+        CreateDirIfInvalid(os.path.join(os.getcwd(), "downloads", "Minecraft_Java"))
+        DownloadFile(f"https://download.oracle.com/java/{version}/latest/jdk-{version}_windows-x64_bin.exe", os.path.join(os.getcwd(), "downloads", "Minecraft_Java"))
+        os.start(os.path.join(os.getcwd(), "downloads", "Minecraft_Java", f"jdk-{version}_windows-x64_bin.exe"))
