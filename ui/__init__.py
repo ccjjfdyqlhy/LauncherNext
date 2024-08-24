1f336504b64175d04fb602aca549c3e74c0254dc
DarkstarXD <ccjjfdyqlhy@outlook.com>
2024-08-24 18:52:13 +0800

    fixed selecting bugs. FINALLY!

--- ui/__init__.py
+++ ui/__init__.py
@@ -16,17 +16,18 @@ import utils.fastgithub as fg_launcher
 
 from .installers import *
 
-__version__='0.0.4'
-forecolor='#FFFFFF'
-bgcolor='#ffffff'
-firstlaunch=False
-cwd=os.getcwd()
+__version__ = '0.0.4'
+forecolor = '#FFFFFF'
+bgcolor = '#ffffff'
+firstlaunch = False
+cwd = os.getcwd()
 app.native.window_args['resizable'] = False
 app.native.start_args['debug'] = False
-app.add_static_files('/static',os.path.join(cwd, "static"))  # Use os.path.join instead of "+"
+app.add_static_files('/static', os.path.join(cwd, "static"))  # Use os.path.join instead of "+"
 config = configparser.ConfigParser()
-logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
 
 class LogElementHandler(logging.Handler):
     def __init__(self, element: ui.log, level: int = logging.NOTSET) -> None:
@@ -40,101 +41,124 @@ class LogElementHandler(logging.Handler):
         except Exception:
             self.handleError(record)
 
+
 class GameCard(ui.card):
     def set_game_name(self, game):
         self._game_name = copy.deepcopy(game)
         self.on("click", lambda: select_game(self._game_name))
 
+
 def onstop():
     fg_launcher.kill()
     app.shutdown()
 
+
 def set_background(color: str) -> None:
     ui.query('body').style(f'background-color: {color}')
+
 
 def set_fgc(event: ValueChangeEventArguments):
     name = type(event.sender).__name__
     if f'{name}: {event.value}' == 'Radio: Grey':
         ui.colors(primary='#555')
-        forecolor='#555'
+        forecolor = '#555'
         config.set('settings', 'forecolor', '#555')
-        with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:config.write(configfile)
+        with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile: config.write(configfile)
     elif f'{name}: {event.value}' == 'Radio: Atlantic':
         ui.colors(primary='#3288AE')
-        forecolor='#3288AE'
+        forecolor = '#3288AE'
         config.set('settings', 'forecolor', '#3288AE')
-        with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:config.write(configfile)
+        with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile: config.write(configfile)
     elif f'{name}: {event.value}' == 'Radio: Forest':
         ui.colors(primary='#346C48')
-        forecolor='#346C48'
+        forecolor = '#346C48'
         config.set('settings', 'forecolor', '#346C48')
-        with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:config.write(configfile)
+        with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile: config.write(configfile)
     elif f'{name}: {event.value}' == 'Radio: Deep Ocean':
         ui.colors(primary='#072A69')
-        forecolor='#072A69'
+        forecolor = '#072A69'
         config.set('settings', 'forecolor', '#072A69')
-        with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:config.write(configfile)
+        with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile: config.write(configfile)
     else:
         ui.colors()
         config.set('settings', 'forecolor', '#5898D4')
-        with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:config.write(configfile)
+        with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile: config.write(configfile)
+
 
 def set_bgc(event: ValueChangeEventArguments):
     name = type(event.sender).__name__
     if f'{name}: {event.value}' == 'Radio: Orange':
         set_background('#ffeedd')
         config.set('settings', 'bgcolor', '#ffeedd')
-        with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:config.write(configfile)
+        with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile: config.write(configfile)
     else:
         set_background('#ffffff')
         config.set('settings', 'bgcolor', '#ffffff')
-        with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:config.write(configfile)
+        with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile: config.write(configfile)
+
 
 def set_pyver():
     pyver = pyverin.value()
 
+
+# --- 全局变量存储选定项 ---
+selected_game = None
+
+
 def select_game(game):
-    logger.info('Instance selected: '+game)
-    game_selected=game
+    global selected_game  # 使用全局变量
+    logger.info('Instance selected: ' + game)
+    selected_game = game
     config.set('apps', 'game_selected', game)
-    with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:config.write(configfile)
-    launch_bt.props(remove='disabled')
-    launch_bt.set_text('启动 '+game)
-    launch_bt.on('click', lambda:xlaunch(game))
-    gamelabel.set_text('选定项目: '+game_selected)
+    with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile: config.write(configfile)
+
+    # 更新按钮和标签
+    update_launch_button()
+    gamelabel.set_text('选定项目: ' + selected_game)
+
+# --- 更新启动按钮的函数 ---
+def update_launch_button():
+    global selected_game # 使用全局变量
+    if selected_game is None:
+        launch_bt.props(remove='disabled') # 移除 disabled 属性
+        launch_bt.set_text('未指定启动项')
+    else:
+        launch_bt.props(remove='disabled')
+        launch_bt.set_text('启动 ' + selected_game)
+
 
 def xlaunch(instance):
-    if not os.path.exists(cwd+'\\apps\\'+instance):
+    if not os.path.exists(cwd + '\\apps\\' + instance):
         logger.warning('Instance folder not found, launching from config file')
         isappfolder = False
     else:
         isappfolder = True
-    logger.info('Getting ready to launch: '+instance)
+    logger.info('Getting ready to launch: ' + instance)
     if isappfolder:
-        if os.path.exists(cwd+'\\apps\\'+instance+'\\'+instance+'.lnxt'):
-            logger.info('Reading '+instance+'.lnxt')
-            config.read(cwd+'\\apps\\'+instance+'\\'+instance+'.lnxt')
+        if os.path.exists(cwd + '\\apps\\' + instance + '\\' + instance + '.lnxt'):
+            logger.info('Reading ' + instance + '.lnxt')
+            config.read(cwd + '\\apps\\' + instance + '\\' + instance + '.lnxt')
         else:
             logger.error('LauncherNext app config file not found.')
             logger.error('Launch terminated.')
             return
     else:
-        if os.path.exists(cwd+'\\apps\\'+instance+'.lnxt'):
-            logger.info('Reading '+instance+'.lnxt')
-            config.read(cwd+'\\apps\\'+instance+'.lnxt')
+        if os.path.exists(cwd + '\\apps\\' + instance + '.lnxt'):
+            logger.info('Reading ' + instance + '.lnxt')
+            config.read(cwd + '\\apps\\' + instance + '.lnxt')
         else:
             logger.error('LauncherNext app config file not found.')
             logger.error('Launch terminated.')
             return
-    
+
     appclass = config.get('app', 'class')
     if appclass == 'exe':
         appexec = config.get('app', 'exec')
     elif appclass == 'jar':
-        appexec = 'java -jar '+config.get('app', 'exec')
+        appexec = 'java -jar ' + config.get('app', 'exec')
     elif appclass == 'minecraft':
         pass
-        #TODO
+        # TODO
     elif appclass == 'py':
         try:
             runtime = config.get('app', 'runtime')
@@ -143,77 +167,78 @@ def xlaunch(instance):
             logger.error('LauncherNext app config file is missing a required option.')
             return
         if runtime == '': runtime = 'python'
-        appexec = runtime+' '+config.get('app', 'exec')
-    logger.info('Launching '+instance)
+        appexec = runtime + ' ' + config.get('app', 'exec')
+    logger.info('Launching ' + instance)
     if appclass == 'py':
-        daemon.exec(appexec,vcwd)
+        daemon.exec(appexec, vcwd)
     else:
         daemon.exec(appexec)
     logger.info('Launch complete.')
 
+
 memory = psutil.virtual_memory()
-mtotal=int((memory.total / 1024 ** 2)//1024)
-mused=int((memory.used / 1024 ** 2)//1024)
-mfree=mtotal-mused
+mtotal = int((memory.total / 1024 ** 2) // 1024)
+mused = int((memory.used / 1024 ** 2) // 1024)
+mfree = mtotal - mused
 
 if os.path.exists('lnxt.ini'):
     if sys.platform == 'win32':
         installed_apps = daemon.get_installed_list_win()
-    config.read('lnxt.ini',encoding='utf-8-sig')
+    config.read('lnxt.ini', encoding='utf-8-sig')
     config.set('apps', 'installed', installed_apps)
-    with open('lnxt.ini', 'w',encoding='utf-8-sig') as configfile:
+    with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile:
         config.write(configfile)
-    config.read('lnxt.ini',encoding='utf-8-sig')
+    config.read('lnxt.ini', encoding='utf-8-sig')
     launchtime = int(config.get('general', 'launch'))
     forecolor = config.get('settings', 'forecolor')
     bgcolor = config.get('settings', 'bgcolor')
     if forecolor == '#555':
-        fgc_name='Grey'
+        fgc_name = 'Grey'
     elif forecolor == '#3288AE':
-        fgc_name='Atlantic'
+        fgc_name = 'Atlantic'
     elif forecolor == '#346C48':
-        fgc_name='Forest'
+        fgc_name = 'Forest'
     elif forecolor == '#072A69':
-        fgc_name='Deep Ocean'
+        fgc_name = 'Deep Ocean'
     else:
-        fgc_name='Defalt'
+        fgc_name = 'Defalt'
     if bgcolor == '#ffeedd':
-        bgc_name='Orange'
+        bgc_name = 'Orange'
     else:
-        bgc_name='Defalt'
+        bgc_name = 'Defalt'
     ui.colors(primary=forecolor)
     set_background(bgcolor)
-    installed_apps= config.get('apps', 'installed').split(',')
-    game_list= config.get('apps', 'game_list').split(',')
-    game_local= config.get('apps', 'game_local').split(',')
-    game_selected=config.get('apps', 'game_selected')
+    installed_apps = config.get('apps', 'installed').split(',')
+    game_list = config.get('apps', 'game_list').split(',')
+    game_local = config.get('apps', 'game_local').split(',')
+    game_selected = config.get('apps', 'game_selected')
     logger.info('Configuration file loaded.')
 else:
     if sys.platform == 'win32':
         installed_apps = daemon.get_installed_list_win()
-    open('lnxt.ini', 'w',encoding='utf-8-sig').close()
+    open('lnxt.ini', 'w', encoding='utf-8-sig').close()
     config['general'] = {
-    "launch": '1'
+        "launch": '1'
     }
     config['settings'] = {
-    "forecolor": '#5898D4',
-    "bgcolor": "#ffffff"
+        "forecolor": '#5898D4',
+        "bgcolor": "#ffffff"
     }
     config['apps'] = {
-    "installed": installed_apps,
-    "game_list": 'MCSA Enchanted,MCSA Enchanted Light,MCSA Multiverse,Minecraft Java,Minecraft Bedrock,Genshin Impact',
-    "game_local": 'None',
-    "game_selected": 'None'
+        "installed": installed_apps,
+        "game_list": 'MCSA Enchanted,MCSA Enchanted Light,MCSA Multiverse,Minecraft Java,Minecraft Bedrock,Genshin Impact',
+        "game_local": 'None',
+        "game_selected": 'None'
     }
     with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile:
         config.write(configfile)
     logger.info('New configuration file created.')
-    fgc_name='Defalt'
-    bgc_name='Defalt'
-    installed_apps=installed_apps.split(',')
-    game_list='MCSA Enchanted,MCSA Enchanted Light,MCSA Multiverse,Minecraft Java,Minecraft Bedrock,Genshin Impact'.split(',')
-    game_local='None'
-    game_selected='None'
+    fgc_name = 'Defalt'
+    bgc_name = 'Defalt'
+    installed_apps = installed_apps.split(',')
+    game_list = 'MCSA Enchanted,MCSA Enchanted Light,MCSA Multiverse,Minecraft Java,Minecraft Bedrock,Genshin Impact'.split(',')
+    game_local = 'None'
+    game_selected = 'None'
     launchtime = 1
 
 if game_selected == 'None':
@@ -231,7 +256,7 @@ if launchtime < 2:
             ui.button('跳过', on_click=dialog.close)
     dialog.open()
 if launchtime % 2 == 0:
-    print('\nLauncherNext Interface '+__version__+'\n')
+    print('\nLauncherNext Interface ' + __version__ + '\n')
     if not daemon.is_alive('fastgithub.exe'):
         fg_launcher.launch()
     else:
@@ -239,7 +264,7 @@ if launchtime % 2 == 0:
 launchtime = launchtime + 1
 config['general'] = {
     "launch": launchtime
-    }
+}
 with open('lnxt.ini', 'w', encoding='utf-8-sig') as configfile:
     config.write(configfile)
 
@@ -266,7 +291,7 @@ with ui.left_drawer().classes('bg-blue-200') as left_drawer:
 with ui.tab_panels(tabs, value='启动面板').classes('w-full'):
     with ui.tab_panel('启动面板'):
         ui.label('启动面板').style('color: #6E93D6; font-size: 200%; font-weight: 300')
-        gamelabel=ui.label('选定项目: '+game_selected)
+        gamelabel = ui.label('选定项目: ' + game_selected)
     with ui.tab_panel('产品库'):
         with ui.column():
             with ui.card():
@@ -277,7 +302,7 @@ with ui.tab_panels(tabs, value='启动面板').classes('w-full'):
                     with GameCard() as card:
                         card.set_game_name(game)
                         with ui.row():
-                            onclick = lambda: gamelabel.set_text('选定项目: '+game)
+                            onclick = lambda game=game: gamelabel.set_text('选定项目: ' + game)  # 使用默认参数
                             ui.label(game)
             with ui.card():
                 ui.label('系统应用').style('font-size: 150%; font-weight: 300')
@@ -294,13 +319,15 @@ with ui.tab_panels(tabs, value='启动面板').classes('w-full'):
                 ui.label('LauncherNext').style('color: #6E93D6; font-size: 200%; font-weight: 300')
                 with ui.column():
                     ui.label('一个基于webUI和Python的轻量级启动器。')
-                    ui.label('当前版本：'+__version__)
+                    ui.label('当前版本：' + __version__)
                     ui.label('由 DarkstarXD 和 Allen546 联合开发。')
                     ui.separator()
                     with ui.row():
                         ui.button('检查更新').props('disabled')
-                        ui.button('许可与版权声明',on_click=lambda:webbrowser.open('https://github.com/ccjjfdyqlhy/LauncherNext/blob/main/LICENSE'))
-                        ui.button('在 Github 上查看此项目',on_click=lambda:webbrowser.open('https://github.com/ccjjfdyqlhy/LauncherNext'))
+                        ui.button('许可与版权声明', on_click=lambda: webbrowser.open(
+                            'https://github.com/ccjjfdyqlhy/LauncherNext/blob/main/LICENSE'))
+                        ui.button('在 Github 上查看此项目',
+                                  on_click=lambda: webbrowser.open('https://github.com/ccjjfdyqlhy/LauncherNext'))
             with ui.card():
                 with ui.column():
                     ui.label('账户').style('font-size: 150%; font-weight: 300')
@@ -310,39 +337,43 @@ with ui.tab_panels(tabs, value='启动面板').classes('w-full'):
         with ui.card():
             ui.label('主题设置').style('font-size: 150%; font-weight: 300')
             ui.label('前景色设置')
-            fgcradio=ui.radio(['Defalt','Atlantic','Forest','Deep Ocean','Grey'],value=fgc_name,on_change=set_fgc).props('inline')
+            fgcradio = ui.radio(['Defalt', 'Atlantic', 'Forest', 'Deep Ocean', 'Grey'], value=fgc_name,
+                                on_change=set_fgc).props('inline')
             ui.label('背景色设置(Beta)')
-            bgcradio=ui.radio(['Defalt','Orange'],value=bgc_name,on_change=set_bgc).props('inline')
+            bgcradio = ui.radio(['Defalt', 'Orange'], value=bgc_name, on_change=set_bgc).props('inline')
         with ui.card():
             ui.label('实例设置').style('font-size: 150%; font-weight: 300')
             ui.label('Python类').style('font-size: 125%')
-            #TODO
+            # TODO
             with ui.column():
                 with ui.row():
-                    pyverin=ui.input("默认Python运行时:")
+                    pyverin = ui.input("默认Python运行时:")
                     ui.button('应用')
             ui.label('Minecraft类').style('font-size: 125%')
             with ui.row():
                 ui.label("安装Java: ").style('margin-top:13px;')
-                javaverin=ui.select(list(launchers.mc_java.JavaManager.versions.keys()), value="17").style("margin-bottom: 15px;padding-right: 10px")
-                ui.button('下载Java',on_click=get_java_installer_onclick(javaverin)).style("margin-top: 10px;")
+                javaverin = ui.select(list(launchers.mc_java.JavaManager.versions.keys()), value="17").style(
+                    "margin-bottom: 15px;padding-right: 10px")
+                ui.button('下载Java', on_click=get_java_installer_onclick(javaverin)).style("margin-top: 10px;")
                 ui.label("Java策略: ").style('margin-top:13px;')
-                javarole=ui.select(['自动根据Minecraft版本选择Java'] + list(JavaManager.versions.keys()), value="自动根据Minecraft版本选择Java").style("margin-bottom: 15px;padding-right: 10px")
+                javarole = ui.select(['自动根据Minecraft版本选择Java'] + list(JavaManager.versions.keys()),
+                                      value="自动根据Minecraft版本选择Java").style(
+                    "margin-bottom: 15px;padding-right: 10px")
             with ui.row():
-                ui.label("JVM内存分配: "+'总内存 '+str(mtotal)+' GB').style('margin-top:13px;')
+                ui.label("JVM内存分配: " + '总内存 ' + str(mtotal) + ' GB').style('margin-top:13px;')
                 slider = ui.slider(min=1, max=mtotal, step=0.5, value=8)
                 ui.number().bind_value(slider)
                 ui.label('GB')
 
 with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
-    if game_selected == '未指定':
-        launch_bt=ui.button('未指定启动项').props('disabled')
-    else:
-        launch_bt=ui.button(text='启动 '+game_selected)
-        launch_bt.on('click', lambda:xlaunch(game_selected))
+    launch_bt = ui.button(
+        on_click=lambda: xlaunch(selected_game) # 使用 lambda 传递选定项
+    )
+    update_launch_button()  # 初始化按钮状态
 
 app.on_disconnect(lambda: onstop())
 ui.context.client.on_disconnect(lambda: logger.removeHandler(handler))
 
+
 def main():
-    ui.run(native=True, window_size=(1280,720), title='LauncherNext Interface', reload=False)
+    ui.run(native=True, window_size=(1280, 720), title='LauncherNext Interface', reload=False)
